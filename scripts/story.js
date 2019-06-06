18f9a9753b1346f9d4fcb88c59da123c8cad3d32
Canman33 <canman33@hotmail.com>
2019-06-06 10:24:10 -0400

    added some new content

--- scripts/story.js
+++ scripts/story.js
@@ -1,14 +1,19 @@
 var verb = prompt('Let\'s write a mad lib, shall we? Enter a verb.');
 var noun = prompt('Great, now enter a noun.');
 var adjective = prompt('one more, enter an adjective.');
+var adjective2 = prompt('one more adjective Yo, Thanks');
 
-var message = ' One day, I was walking through the ' + verb + ',';
-message +=  ' when I came upon a ' + noun + ' ,';
-message += ' frightened, I turned and ran straight into an ' + adjective + '.';
+var message = ' One day, I was ' + verb + ' in the woods ';
+message +=  ' when I came upon a ' + noun + ',';
+message += ' frightened, I turned and ran straight into a ' + adjective + '.';
+message += ' the smell of ' + adjective + ' was in the air';
 alert('We\'ve made the story\! Now, Here it is.');
 alert('But, before i forget..., go Efff yourself! HAAAAAA!');
-alert('also, there\'s now a bug embedded on your device. ')
+alert('if you\'d like to learn programming, click on the treehouse link');
+
 document.write("<p style='font-size:2.5em'>" + message + "</p>");
+
+
 
 /*initialized a git repository and pushed, but for some reason
 it won't display in github pages, because(from what i can tell)
